9abaf811b1292f9df9129528180d8a4bda983b18
abhitripathi273 <abhitripathi2003@gmail.com>
2023-04-18 16:05:40 +0530

    updated

--- src/main/java/com/emp/attendanceservice/AttendanceServiceApplication.java
+++ src/main/java/com/emp/attendanceservice/AttendanceServiceApplication.java
@@ -12,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient 
 public class AttendanceServiceApplication {
-
 	/**
 	 * The main method.
 	 *
