8098b9671173f3cd9202b7de50d2a7dded2d39b9
abhitripathi273 <abhitripathi2003@gmail.com>
2023-04-19 08:52:18 +0530

    some commit

--- src/main/java/com/emp/attendanceservice/AttendanceServiceApplication.java
+++ src/main/java/com/emp/attendanceservice/AttendanceServiceApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 /**
  * The Class AttendanceServiceApplication.
  */
@@ -17,6 +15,7 @@ public class AttendanceServiceApplication {
 	 *
 	 * @param args the arguments
 	 */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceServiceApplication.class, args);
 	}
